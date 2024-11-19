1d92323e48afa9db152cb86ae244305f2048f4b5
Afzal <fmafzalulbare@gmail.com>
2024-11-19 14:05:54 +0600

    text media block

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,30 @@ module.exports = {
       fontFamily: {
         lato: ["Lato", "sans-serif"],
       },
+      fontSize: {
+        44: "2.75rem",
+      },
       maxWidth: {
         siteWidth: "1512px",
       },
       colors: {
+        'black': "#344054",
         'blue1': "#266df1",
         'gray1': "#e9e9e4",
         'blue-light': "#568df4",
         'bright-Orange': "#FF742D",
         'soft-vanilla': "#FFF5D2",
+      },
+      padding: {
+        30: "30px",
+        50: "50px",
+      },
+      lineHeight: {
+        '22': '1.375rem',
+        '32': '2rem',
+      },
+      letterSpacing: {
+        '1.5': '0.09375rem',
       },
     },
   },
