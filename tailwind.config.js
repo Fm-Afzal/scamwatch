8eb8a280b18f58e02d47fda8bf63078d22b3d459
Afzal <fmafzalulbare@gmail.com>
2024-11-19 12:20:51 +0600

    menu link added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
       maxWidth: {
         siteWidth: "1512px",
       },
+      colors: {
+        'blue1': "#266df1",
+        'gray1': "#e9e9e4",
+        'blue-light': "#568df4",
+        'bright-Orange': "#FF742D",
+        'soft-vanilla': "#FFF5D2",
+      },
     },
   },
   plugins: [],
